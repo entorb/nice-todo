eb071916f01320d10046d64190426b69e85fa350
Torben <59419684+entorb@users.noreply.github.com>
2026-06-13 20:57:21 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,16 +10,15 @@ set -e
 
 DEPS=$(uv run python -c "import tomllib; d=tomllib.load(open('pyproject.toml','rb')); print(' '.join(p.split('>')[0].split('<')[0].split('=')[0].split('!')[0] for p in d['project']['dependencies']))")
 DEV_DEPS=$(uv run python -c "import tomllib; d=tomllib.load(open('pyproject.toml','rb')); print(' '.join(p.split('>')[0].split('<')[0].split('=')[0].split('!')[0] for p in d['dependency-groups']['dev']))")
-DEPS_VERSIONED=$(uv run python -c "import tomllib; d=tomllib.load(open('pyproject.toml','rb')); print(' '.join(d['project']['dependencies']))")
-DEV_DEPS_VERSIONED=$(uv run python -c "import tomllib; d=tomllib.load(open('pyproject.toml','rb')); print(' '.join(d['dependency-groups']['dev']))")
 
 # 1. remove all
 uv remove $DEPS
 uv remove --dev $DEV_DEPS
 
-# 2. re-add with pinned versions from pyproject.toml (sets constraints)
-uv add $DEPS_VERSIONED
-uv add --dev $DEV_DEPS_VERSIONED
+# 2. re-add
+# uv add numpy==2.2.3 pandas==2.2.3 pyarrow==20.0.0
+uv add $DEPS
+uv add --dev $DEV_DEPS
 
 # 3. upgrade within constraints
 uv lock --upgrade
